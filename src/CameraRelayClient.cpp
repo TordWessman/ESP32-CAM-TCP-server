d7f93b5eea41283330accad759edb70bfc1a8271
Tord Wessman <tord@axelit.se>
2026-01-05 14:35:17 +0100

    reduce image quality and size for relay client

--- src/CameraRelayClient.cpp
+++ src/CameraRelayClient.cpp
@@ -71,17 +71,17 @@ bool CameraRelayClient::initCamera() {
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
-    config.frame_size = FRAMESIZE_UXGA;
+    config.frame_size = FRAMESIZE_VGA;
     config.pixel_format = PIXFORMAT_JPEG;
     config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
     config.fb_location = CAMERA_FB_IN_PSRAM;
-    config.jpeg_quality = 12;
+    config.jpeg_quality = 15;
     config.fb_count = 1;
-    
+
     // Optimize settings based on PSRAM availability
     if (config.pixel_format == PIXFORMAT_JPEG) {
         if (psramFound()) {
-            config.jpeg_quality = 10;
+            config.jpeg_quality = 15;
             config.fb_count = 2;
             config.grab_mode = CAMERA_GRAB_LATEST;
         } else {
